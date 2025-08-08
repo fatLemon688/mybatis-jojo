78419a9885f36f465c7ef4dd65bb09473ac1df44
fatLemon688 <hu468502553@qq.com>
2025-08-08 10:12:47 +0800

    feat JDBC事务

---
+++ src/main/java/com/jojo/mybatis/transaction/JDBCTransaction.java
@@ -0,0 +1,56 @@
+package com.jojo.mybatis.transaction;
+
+import lombok.SneakyThrows;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+
+/**
+ *  JDBC事务管理
+ */
+public class JDBCTransaction implements Transaction {
+
+    private DataSource dataSource;
+
+    private Connection connection;
+
+    private boolean autoCommit;
+
+    public JDBCTransaction(DataSource dataSource, boolean autoCommit) {
+        this.dataSource = dataSource;
+        this.autoCommit = autoCommit;
+    }
+
+    @SneakyThrows
+    @Override
+    public Connection getConnection() {
+        Connection connection = dataSource.getConnection();
+        connection.setAutoCommit(autoCommit);
+        this.connection = connection;
+        return connection;
+    }
+
+    @SneakyThrows
+    @Override
+    public void commit() {
+        if (autoCommit) {
+            return;
+        }
+        connection.commit();
+    }
+
+    @SneakyThrows
+    @Override
+    public void rollback() {
+        if (autoCommit) {
+            return;
+        }
+        connection.rollback();
+    }
+
+    @SneakyThrows
+    @Override
+    public void close() {
+        connection.close();
+    }
+}
